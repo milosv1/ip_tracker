4d588b49be26bcbed8b8a9381bbbec8eaaa2d698
Milos Vuksanovic <milosvuksanovic53@gmail.com>
2020-08-04 20:03:30 +1000

    Project file added with first comment & objective.

---
+++ network_tracer.py
@@ -0,0 +1,2 @@
+#first commit of the project, essentially it will be a script or potentially a new side project - which tracks ip addresses, when a new -
+#address connects it will send through an email or another form of notification to say that a new ip-addr. has made connection.
