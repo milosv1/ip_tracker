efd77b762bbd13743801bbf6ae353d847f8c3c44
Milos Vuksanovic <milosvuksanovic53@gmail.com>
2020-08-17 16:27:29 +1000

    Removed functions, placed into get_args() function

--- network_tracer.py
+++ network_tracer.py
@@ -5,19 +5,24 @@ import subprocess
 import os
 import socket
 import uuid
+import argparse
 
 
-def get_mac():  
-    print("MAC address: ", end="")
-    mac_add = print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
-    for ele in range(0,8*6,8)][::-1])) 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-mi","--my_ip")
+    parser.add_argument("-ma","--my_mac")
+    args = parser.parse_args()
+    if args.my_ip:
+        print(f"{args.my_ip} works!")
+        ip_ad = socket.gethostbyname(socket.gethostname())
+        print("IP: ", ip_ad)
+    elif args.my_mac:
+          print("MAC address: ", end="")
+          mac_add = print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
+          for ele in range(0,8*6,8)][::-1])) 
 
 
-def get_ip():
-    ip_ad = socket.gethostbyname(socket.gethostname())
-    print("IP: ", ip_ad)
 
 
-
-get_mac()
-get_ip()
+get_args()
